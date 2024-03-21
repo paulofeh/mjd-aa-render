3a059197846f160973418bbc23daeb486a0099a5
Paulo Fehlauer <paulofeh@gmail.com>
2024-03-20 22:23:06 -0300

    add telegram bot

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, request
+import requests
+
 
 app = Flask(__name__)
+BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 
 @app.route('/')
 def index():
   return 'Olá, <b>tudo bem?</b>'
 
-@app.route('/sobre')
+@app.route('/teste')
 def sobre():
   return 'Sobre o <strong>projeto</strong>'
+
+@app.route('/telegram', methods="POST")
+def telegram_update():
+  update = request.json
+  url_envio_mensagem = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+  chat_id = update['message']['chat']['id']
+  mensagem = {'chat_id': chat_id, 'text': 'Mensagem <b>recebida</b>', 'parse_mode': 'html'}
+  request.post(url_envio_mensagem, data=mensagem)
+  return 'ok'
